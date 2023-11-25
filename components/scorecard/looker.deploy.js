5cafb4f55906ce4b7738ad51b7ca00dc9073a89d
Brian Buie <brian@buie.dev>
2023-11-25 15:48:13 -0600

    simplify config and deployment

--- components/scorecard/looker.deploy.js
+++ components/scorecard/looker.deploy.js
@@ -1,3 +1,11 @@
+import fs from 'fs';
+import { execSync } from 'child_process';
+
+/**
+ * Basic Component Info
+ * Differs for stage vs production
+ */
+
 const env = {
   name: '[Dev] Scorecard',
   description: 'A flexible scorecard component',
@@ -8,6 +16,13 @@ if (process.env.NODE_ENV === 'production') {
   env.name = 'Scorecard';
   env.bucket = 'gs://bbuie-looker/scorecard';
 }
+
+console.log(`\nDeploying "${env.name}" to ${env.bucket}...\n`);
+
+/**
+ * manifest.json
+ * Required for looker components
+ */
 
 const manifest = {
   name: env.name,
@@ -24,6 +39,13 @@ const manifest = {
     },
   ],
 };
+
+fs.writeFileSync('dist/manifest.json', JSON.stringify(manifest, null, 2));
+
+/**
+ * Component input definition
+ * Outlines the interface in Looker Studio for configuring the component
+ */
 
 const input = {
   data: [
@@ -91,4 +113,9 @@ const input = {
   ],
 };
 
-export default { env, manifest, input };
+fs.writeFileSync('dist/input.json', JSON.stringify(input, null, 2));
+
+/**
+ * Copy everything in the dist/ folder to the Google cloud storage bucket
+ */
+execSync(`gsutil -m cp dist/* ${env.bucket}`);
